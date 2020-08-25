3413072bdcd852a9cb92dc0ab3c0b82fc8944d60
Valery Volgutov <valerii76@gmail.com>
2020-08-25 23:02:23 +0300

    First demo
    choose input/output device
    record/play audio
    
    1. Need to prepare audio sample. Press "Record start"
    2. When completed - press "Record stop"
    3. Play recorded audio sample. Press "Play"
    4. You can choose output device in list before play
    5. You can choose input device before record audio

--- app/src/main/java/com/example/demobluetooth/MainActivity.kt
+++ app/src/main/java/com/example/demobluetooth/MainActivity.kt
@@ -1,29 +1,33 @@
 package com.example.demobluetooth
 
 import android.content.Context
-import android.database.DataSetObserver
-import android.media.AudioDeviceInfo
-import android.media.AudioManager
-import android.media.MicrophoneInfo
+import android.media.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import kotlin.concurrent.thread
 
 
 private class InputDeviceListAdapter(val context: Context): BaseAdapter() {
 
+    var selectedItem: Int = -1
     val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     var audioManager: AudioManager? = null
-    var mics: List<MicrophoneInfo> = listOf()
+    var mics: List<AudioDeviceInfo> = listOf()
 
     fun update() {
         audioManager = context.getSystemService(AppCompatActivity.AUDIO_SERVICE) as AudioManager?
-        mics = audioManager?.microphones!!
+        mics = audioManager?.getDevices(AudioManager.GET_DEVICES_INPUTS)!!.toList()
+        selectedItem = -1
         notifyDataSetChanged()
     }
+
 
     override fun getCount(): Int {
         return mics.size
@@ -44,17 +48,19 @@ private class InputDeviceListAdapter(val context: Context): BaseAdapter() {
         val address = rowView.findViewById<TextView>(R.id.lstInputAddress)
         val description = rowView.findViewById<TextView>(R.id.lstInputDescription)
 
-        val mic = getItem(index) as MicrophoneInfo
+        val mic = getItem(index) as AudioDeviceInfo
         type.text = mic.type.toString()
         id.text = mic.id.toString()
         address.text = mic.address
-        description.text = mic.description
+        description.text = mic.productName
 
         return rowView
     }
 }
 
 private class OutputDeviceListAdapter(val context: Context): BaseAdapter() {
+
+    var selectedItem: Int = -1
     val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     var audioManager: AudioManager? = null
     var outputDevices: List<AudioDeviceInfo> = listOf()
@@ -62,6 +68,7 @@ private class OutputDeviceListAdapter(val context: Context): BaseAdapter() {
     fun update() {
         audioManager = context.getSystemService(AppCompatActivity.AUDIO_SERVICE) as AudioManager?
         outputDevices = audioManager?.getDevices(AudioManager.GET_DEVICES_OUTPUTS)!!.toList()
+        selectedItem = -1
         notifyDataSetChanged()
     }
 
@@ -96,10 +103,15 @@ private class OutputDeviceListAdapter(val context: Context): BaseAdapter() {
 
 class MainActivity : AppCompatActivity() {
 
-    var lstInput: ListView? = null
-    var lstOutput: ListView? = null
-    private var inputAdapter: InputDeviceListAdapter? = null
-    private var outputAdapter: OutputDeviceListAdapter? = null
+    private val TAG: String = "BluetoothDemo"
+
+    private lateinit var lstInput: ListView
+    private lateinit var lstOutput: ListView
+    private lateinit var inputAdapter: InputDeviceListAdapter
+    private lateinit var outputAdapter: OutputDeviceListAdapter
+    private lateinit var audioRecorder: AudioRecord
+    private lateinit var audioPlayer: AudioTrack
+    private var micRecord: ByteArrayOutputStream = ByteArrayOutputStream()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -115,14 +127,97 @@ class MainActivity : AppCompatActivity() {
         val btnStop = findViewById<Button>(R.id.btnStopRecord)
         val btnPlay = findViewById<Button>(R.id.btnPlayRecord)
 
-        lstInput?.adapter = inputAdapter
-        lstOutput?.adapter = outputAdapter
+        lstInput.adapter = inputAdapter
+        lstOutput.adapter = outputAdapter
 
-        btnUpdate.setOnClickListener { v ->
-            run {
-                inputAdapter?.update()
-                outputAdapter?.update()
+        lstInput.setOnItemClickListener { _, view, index, _ -> run {
+            view.isSelected = true
+            inputAdapter.selectedItem = index
+        }}
+        lstOutput.setOnItemClickListener {_, view, index, _ -> run {
+            view.isSelected = true
+            outputAdapter.selectedItem = index
+        }}
+
+        btnUpdate.setOnClickListener { run {
+            inputAdapter.update()
+            outputAdapter.update()
+        } }
+
+        btnStart.setOnClickListener { run {
+            // Set preferred device
+            if (inputAdapter.selectedItem != -1) {
+                val device = inputAdapter.getItem(inputAdapter.selectedItem) as AudioDeviceInfo
+                audioRecorder.preferredDevice = device
             }
-        }
+
+            micRecord.reset()
+            audioRecorder.startRecording()
+            thread (start = true) {
+                var totalSize = 0
+                val bufferSize = 1024 * 16
+                val buffer = ByteArray(bufferSize)
+                while (audioRecorder.recordingState == AudioRecord.RECORDSTATE_RECORDING) {
+                    val readCount = audioRecorder.read(buffer,0, bufferSize)
+                    totalSize += readCount
+                    micRecord.write(buffer, 0, readCount)
+                    Log.d(TAG, "readCount: $readCount, totalSize: $totalSize")
+                }
+            }
+        } }
+
+        btnStop.setOnClickListener { run {
+            audioRecorder.stop()
+        } }
+
+        btnPlay.setOnClickListener { run {
+            // Set preferred device
+            if (outputAdapter.selectedItem != -1) {
+                val device = outputAdapter.getItem(outputAdapter.selectedItem) as AudioDeviceInfo
+                audioPlayer.preferredDevice = device
+            }
+
+            val inputBuffer = micRecord.toByteArray()
+            val data = ByteArrayInputStream(inputBuffer)
+            audioPlayer.play()
+
+            var readSize = 0
+            val size = 1024*16
+            val buffer = ByteArray(size)
+            while (readSize != -1) {
+                readSize = data.read(buffer)
+                if (readSize != -1)
+                    audioPlayer.write(buffer, 0, readSize)
+            }
+
+            audioPlayer.stop()
+            Toast.makeText(this@MainActivity, "Play stopped", Toast.LENGTH_LONG).show()
+        } }
+
+        inputAdapter.update()
+        outputAdapter.update()
+
+        // Create AudioRecorder
+        val inputSampleRate = 8000
+        audioRecorder = AudioRecord.Builder()
+            .setAudioSource(MediaRecorder.AudioSource.MIC)
+            .setAudioFormat(AudioFormat.Builder()
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setSampleRate(inputSampleRate)
+                .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
+                .build())
+            .setBufferSizeInBytes(4 * AudioRecord.getMinBufferSize(inputSampleRate,AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT))
+            .build()
+
+        // Create AudioTrack (audio player)
+        val outputSampleRate = 8000
+        audioPlayer = AudioTrack.Builder()
+            .setAudioFormat(AudioFormat.Builder()
+                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setSampleRate(outputSampleRate)
+                .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
+                .build())
+            .setBufferSizeInBytes(4 * AudioRecord.getMinBufferSize(outputSampleRate,AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT))
+            .build()
     }
 }
