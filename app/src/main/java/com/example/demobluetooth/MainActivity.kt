36ec15eb46ee4e4778555f291ccd8431b4ffce58
Valery Volgutov <valerii76@gmail.com>
2020-08-26 23:19:20 +0300

    Hook on handset connection. And async playing

--- app/src/main/java/com/example/demobluetooth/MainActivity.kt
+++ app/src/main/java/com/example/demobluetooth/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.demobluetooth
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.media.*
 import androidx.appcompat.app.AppCompatActivity
@@ -104,6 +110,47 @@ private class OutputDeviceListAdapter(val context: Context): BaseAdapter() {
     }
 }
 
+private class BluetoothHeadsetStateMonitor: BroadcastReceiver()
+{
+    private var isHeadsetConnected: Boolean = false
+    private var listener: ((Boolean) -> Unit)? = null
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val connectionState = intent?.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE, -1)
+        if (connectionState == BluetoothAdapter.STATE_CONNECTED)
+            setHeadsetConnected(true)
+        else if (connectionState == BluetoothAdapter.STATE_DISCONNECTED)
+            setHeadsetConnected(false)
+    }
+
+    fun setHeadsetConnected(headsetConnected: Boolean) {
+        if (isHeadsetConnected != headsetConnected) {
+            isHeadsetConnected = headsetConnected
+            listener?.invoke(isHeadsetConnected)
+        }
+    }
+
+    fun startMonitoring(context: Context, l: (Boolean)->Unit) {
+        listener = l
+        val bluetoothManager = context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager?.adapter.getProfileProxy(context, object :
+            BluetoothProfile.ServiceListener {
+            override fun onServiceConnected(i: Int, profile: BluetoothProfile?) {
+                setHeadsetConnected(if(profile != null) profile.connectedDevices.size > 0 else false)
+            }
+
+            override fun onServiceDisconnected(i: Int) {
+                setHeadsetConnected(false)
+            }
+        }, BluetoothProfile.HEADSET)
+        context.registerReceiver(this, IntentFilter(BluetoothAdapter.ACTION_CONNECTION_STATE_CHANGED))
+    }
+
+    fun stopMonitoring(context: Context) {
+        context.unregisterReceiver(this)
+    }
+}
+
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = "BluetoothDemo"
@@ -116,10 +163,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var audioRecorder: AudioRecord
     private lateinit var audioPlayer: AudioTrack
     private var micRecord: ByteArrayOutputStream = ByteArrayOutputStream()
+    private lateinit var headsetMonitor: BluetoothHeadsetStateMonitor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager?
+
+        headsetMonitor = BluetoothHeadsetStateMonitor()
 
         inputAdapter = InputDeviceListAdapter(this)
         outputAdapter = OutputDeviceListAdapter(this)
@@ -141,6 +193,8 @@ class MainActivity : AppCompatActivity() {
         lstOutput.setOnItemClickListener {_, view, index, _ -> run {
             view.isSelected = true
             outputAdapter.selectedItem = index
+            val device = outputAdapter.getItem(index) as AudioDeviceInfo
+            audioPlayer.preferredDevice = device
         }}
 
         btnUpdate.setOnClickListener { run {
@@ -152,6 +206,11 @@ class MainActivity : AppCompatActivity() {
             // Set preferred device
             if (inputAdapter.selectedItem != -1) {
                 val device = inputAdapter.getItem(inputAdapter.selectedItem) as AudioDeviceInfo
+
+                audioManager?.stopBluetoothSco()
+                if (device.type == AudioDeviceInfo.TYPE_BLUETOOTH_SCO)
+                    audioManager?.startBluetoothSco()
+
                 audioRecorder.preferredDevice = device
             }
 
@@ -172,6 +231,7 @@ class MainActivity : AppCompatActivity() {
 
         btnStop.setOnClickListener { run {
             audioRecorder.stop()
+            audioManager?.stopBluetoothSco()
         } }
 
         btnPlay.setOnClickListener { run {
@@ -181,21 +241,26 @@ class MainActivity : AppCompatActivity() {
                 audioPlayer.preferredDevice = device
             }
 
-            val inputBuffer = micRecord.toByteArray()
-            val data = ByteArrayInputStream(inputBuffer)
-            audioPlayer.play()
+            thread(start = true) {
+                val inputBuffer = micRecord.toByteArray()
+                val data = ByteArrayInputStream(inputBuffer)
+                audioPlayer.play()
 
-            var readSize = 0
-            val size = 1024*16
-            val buffer = ByteArray(size)
-            while (readSize != -1) {
-                readSize = data.read(buffer)
-                if (readSize != -1)
-                    audioPlayer.write(buffer, 0, readSize)
+                var readSize = 0
+                val size = 1024 * 16
+                val buffer = ByteArray(size)
+                while (readSize != -1) {
+                    readSize = data.read(buffer)
+                    if (readSize != -1)
+                        audioPlayer.write(buffer, 0, readSize)
+                }
+
+                audioPlayer.stop()
+
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Play stopped", Toast.LENGTH_LONG).show()
+                }
             }
-
-            audioPlayer.stop()
-            Toast.makeText(this@MainActivity, "Play stopped", Toast.LENGTH_LONG).show()
         } }
 
         inputAdapter.update()
@@ -206,7 +271,9 @@ class MainActivity : AppCompatActivity() {
                 // All ok...
             }
             else -> {
-                requestPermissions(arrayOf(android.Manifest.permission.RECORD_AUDIO), PERMISSION_REQUEST_CODE)
+                requestPermissions(arrayOf(android.Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                    android.Manifest.permission.BLUETOOTH,
+                    android.Manifest.permission.RECORD_AUDIO), PERMISSION_REQUEST_CODE)
             }
         }
 
@@ -215,23 +282,46 @@ class MainActivity : AppCompatActivity() {
         audioRecorder = AudioRecord.Builder()
             .setAudioSource(MediaRecorder.AudioSource.MIC)
             .setAudioFormat(AudioFormat.Builder()
-                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setEncoding(AudioFormat.ENCODING_PCM_8BIT)
                 .setSampleRate(inputSampleRate)
                 .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
                 .build())
-            .setBufferSizeInBytes(4 * AudioRecord.getMinBufferSize(inputSampleRate,AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT))
+            .setBufferSizeInBytes(4 * AudioRecord.getMinBufferSize(inputSampleRate,AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_8BIT))
             .build()
 
         // Create AudioTrack (audio player)
         val outputSampleRate = 8000
         audioPlayer = AudioTrack.Builder()
             .setAudioFormat(AudioFormat.Builder()
-                .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+                .setEncoding(AudioFormat.ENCODING_PCM_8BIT)
                 .setSampleRate(outputSampleRate)
                 .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
                 .build())
-            .setBufferSizeInBytes(4 * AudioRecord.getMinBufferSize(outputSampleRate,AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_16BIT))
+            .setBufferSizeInBytes(4 * AudioRecord.getMinBufferSize(outputSampleRate,AudioFormat.CHANNEL_IN_STEREO, AudioFormat.ENCODING_PCM_8BIT))
             .build()
+
+        headsetMonitor.startMonitoring(this) { connected ->
+            inputAdapter.update()
+            outputAdapter.update()
+            if (connected) {
+                audioManager?.startBluetoothSco()
+            }
+            else
+                audioManager?.stopBluetoothSco()
+        }
+
+        val filter = IntentFilter()
+        filter.addAction(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED)
+        registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (intent!!.action.equals(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED)) {
+                    val audioState = intent.getIntExtra(AudioManager.EXTRA_SCO_AUDIO_STATE, -1)
+                    if (audioState == AudioManager.SCO_AUDIO_STATE_CONNECTED) {
+                        audioManager?.isBluetoothScoOn = true
+                    }
+                }
+            }
+        }, filter)
     }
 
     override fun onRequestPermissionsResult(
